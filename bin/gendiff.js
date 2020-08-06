4f097773680f43735204fdc8cbc353e1408a0952
Mapuk <mar-valshin@yandex.ru>
2020-08-07 00:16:04 +0400

    fix eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import genDiff from '../src/index.js';
 import program from 'commander';
+import genDiff from '../src/index.js';
 
-program.description('Compares two configuration files and shows a difference.')
-
-program.arguments('<filepath1> <filepath2>')
-.action(function (filepath1, filepath2) {   
-    const diff = genDiff(filepath1, filepath2)
-    console.log(diff);
-});
-
-program.version(true)
+program.description('Compares two configuration files and shows a difference.');
 
 program
-.option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
-program.parse(process.argv)
+program.version(true);
+
+program.option('-f, --format [type]', 'output format');
+
+program.parse(process.argv);
